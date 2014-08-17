83568784784b1a0f37d1277675cf8ba0818743b6
Tarik Onalan <phi.quantum@gmail.com>
2014-08-17 23:22:08 +0300

    Currently receiving 400 bad request error

--- sync.rb
+++ sync.rb
@@ -1,5 +1,13 @@
-require 'rest-client'
 require 'json'
+require 'rest-client'
 
 CLIENT_ID = ENV['GH_BASIC_CLIENT_ID']
 CLIENT_SECRET = ENV['GH_BASIC_SECRET_ID']
+
+def auth(username, password)
+    response = RestClient.post "https://#{username}:#{password}@api.github.com/authorizations",
+                                { :params => { 'scopes' => ['user', 'repo'], 'note' => 'octocat-sync' } }
+    puts response;
+end
+
+auth()
