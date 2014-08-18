7cda9a5474b1d18e3983c2fb430da21f1d1db176
Tarik Onalan <phi.quantum@gmail.com>
2014-08-18 11:46:55 +0300

    Token authentication

--- main.rb
+++ main.rb
@@ -3,19 +3,34 @@ require 'rest-client'
 
 HOME = ENV["HOME"]
 
-$token
+$token = ""
 
-def checkauth()
-    return File.exist?("#{HOME}/.config/sync")
+def authenticated()
+    return File.exist?("#{HOME}/.config/gitconfig-sync")
 end
 
-def auth(username, password)
-    response = JSON.parse(RestClient.post("https://#{username}:#{password}@api.github.com/authorizations",
-               { :scopes => ["user", "repo"], :note => "sync" }.to_json, :accept => :json))
-    $token = response["token"]
+def authenticate(username, password)
+    if authenticated
+        $token = [*File.open("#{HOME}/.config/gitconfig-sync")][2].split(/\:\s/)[1]
+    else
+        $token = JSON.parse(RestClient.post("https://#{username}:#{password}@api.github.com/authorizations",
+                    { :scopes => ["user", "repo"], :note => "gitconfig-sync" }.to_json, :accept => :json))["token"]
+        File.open("#{HOME}/.config/gitconfig-sync", "w") do |file|
+            file.write("github.com:\n\tuser: #{username}\n\toauth-token: #$token")
+        end
+    end
 end
 
 def sync()
+    puts "creating repo"
+    result = RestClient.post("https://#$token:x-oauth-basic@api.github.com/user/repos",
+                { :name => "gitconfig", :description => "My gitconfig", :auto_init => true }.to_json, :accept => :json)
+    if result.response == 201
+        result = JSON.parse(result)
+    else
+        puts "error:\n#{result}"
+    end
 
+end
 
 if __FILE__ == $0
